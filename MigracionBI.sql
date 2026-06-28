73e7b75f656b302409238407dea52f83376c41cd
BrunoRodriguezzz <brrodriguez@frba.utn.edu.ar>
2026-06-28 19:24:50 -0300

    Vistas completas

--- MigracionBI.sql
+++ MigracionBI.sql
@@ -341,3 +341,162 @@ SELECT 'Venta', COUNT(*), (SELECT COUNT(*) FROM Hecho_Venta) FROM QUEQUE.Venta
 UNION ALL
 SELECT 'ValoracionEncuesta', COUNT(*), (SELECT COUNT(*) FROM Hecho_Valoracion_Encuesta) FROM QUEQUE.ValoracionEncuesta;
 GO
+
+-- Vista 1
+CREATE VIEW BI_Ticket_Promedio AS
+SELECT 
+    t.Año,
+    t.Mes,
+    rc.desde AS Cliente_Edad_Desde,
+    rc.hasta AS Cliente_Edad_Hasta,
+    cv.Descripcion AS Canal_Venta,
+    str(AVG(v.importe_total),15,2) AS Ticket_Promedio
+FROM Hecho_Venta v
+JOIN DIM_Tiempo t ON v.Tiempo = t.ID
+JOIN DIM_RangoECliente rc ON v.RangoEtarioCliente = rc.ID
+JOIN DIM_CanalVenta cv ON v.CanalVenta = cv.id_canal
+GROUP BY 
+    t.Año, 
+    t.Mes, 
+    rc.desde, 
+    rc.hasta, 
+    cv.Descripcion;
+GO 
+
+-- Vista 2
+CREATE VIEW BI_Distribucion_Facturacion AS
+SELECT 
+    t.Año,
+    t.Cuatrimestre,
+    ts.descripcion AS Tipo_Servicio,
+    SUM(v.importe_total) AS Facturacion_Absoluta,
+    str(SUM(v.importe_total) * 100 /
+        (SELECT SUM(v2.importe_total) FROM Hecho_Venta v2 
+            JOIN DIM_Tiempo t2 ON v2.Tiempo = t2.ID WHERE t2.Año = t.Año AND t2.Cuatrimestre = t.Cuatrimestre),15,2) AS Porcentaje_Facturacion
+FROM Hecho_Venta v
+JOIN DIM_Tiempo t ON v.Tiempo = t.ID
+JOIN DIM_TipoServicio ts ON v.TipoServicio = ts.ID
+GROUP BY 
+    t.Año, 
+    t.Cuatrimestre, 
+    ts.descripcion;
+GO 
+
+-- Vista 3
+
+CREATE VIEW BI_Ranking_Solicitudes_Temporada AS
+SELECT 
+    t.Año,
+    temp.descripcion AS Temporada,
+    rc.desde AS Cliente_Edad_Desde,
+    rc.hasta AS Cliente_Edad_Hasta,
+    COUNT(s.id_solicitud) AS Cantidad_Solicitudes
+FROM Hecho_Solicitud s
+JOIN DIM_Tiempo t ON s.Tiempo = t.ID
+JOIN DIM_Temporada temp ON s.Temporada = temp.ID
+JOIN DIM_RangoECliente rc ON s.RangoEtarioCliente = rc.ID
+GROUP BY 
+    t.Año,
+    temp.descripcion,
+    rc.desde,
+    rc.hasta;
+GO
+
+-- Vista 4
+
+CREATE VIEW BI_Anticipacion_Promedio_Solicitudes AS
+SELECT 
+    t.Año,
+    t.Cuatrimestre,
+    rc.desde AS Cliente_Edad_Desde,
+    rc.hasta AS Cliente_Edad_Hasta,
+    AVG(DATEDIFF(DAY, s.fecha_solicitud, s.fecha_inicio_tentativa)) AS Anticipacion_Promedio_Dias
+FROM Hecho_Solicitud s
+JOIN DIM_Tiempo t ON s.Tiempo = t.ID
+JOIN DIM_RangoECliente rc ON s.RangoEtarioCliente = rc.ID
+GROUP BY 
+    t.Año,
+    t.Cuatrimestre,
+    rc.desde,
+    rc.hasta;
+GO
+
+-- Vista 5
+
+CREATE VIEW BI_Tasa_Aceptacion_Propuestas AS
+SELECT 
+    t.Año,
+    t.Cuatrimestre,
+    ep.Descripcion AS Estado_Propuesta,
+    COUNT(p.id_propuesta) AS Cantidad_Absoluta,
+    str((COUNT(p.id_propuesta) * 100.0 / (SELECT COUNT(p2.id_propuesta) FROM Hecho_Propuesta p2 JOIN DIM_Tiempo t2 ON p2.Tiempo = t2.ID WHERE t2.Año = t.Año AND t2.Cuatrimestre = t.Cuatrimestre)),5,2) AS Tasa_Porcentaje
+
+FROM Hecho_Propuesta p
+JOIN DIM_Tiempo t ON p.Tiempo = t.ID
+JOIN DIM_EstadoPropuesta ep ON p.EstadoPropuesta = ep.id_estado
+GROUP BY 
+    t.Año,
+    t.Cuatrimestre,
+    ep.Descripcion;
+GO
+
+-- Vista 6
+
+CREATE VIEW BI_Cotizacion_Promedio_Temporada AS
+SELECT 
+    t.Año,
+    temp.descripcion AS Temporada,
+    str(AVG(p.importe_total),18,2) AS Cotizacion_Promedio
+FROM Hecho_Propuesta p
+JOIN DIM_Tiempo t ON p.Tiempo = t.ID
+JOIN DIM_Temporada temp ON p.Temporada = temp.ID
+GROUP BY 
+    t.Año,
+    temp.descripcion;
+GO
+
+-- Vista 7 - Tiempo promedio de respuesta
+
+CREATE VIEW BI_Tiempo_promedio_de_respuesta AS
+SELECT
+    h.RangoEtarioAgente,
+    t.Mes,
+    AVG(h.tiempo_respuesta_dias) promedio_tiempo_respuesta
+FROM Hecho_Propuesta h
+JOIN DIM_Tiempo t on h.Tiempo = t.ID
+GROUP BY h.RangoEtarioAgente, t.Mes
+GO
+
+-- Vista 8 - Desvío de presupuesto
+
+CREATE VIEW BI_Desvio_de_presupuesto AS
+SELECT
+    AVG(h.desvio_presupuesto) promedio_desvio_presupuesto
+FROM Hecho_Propuesta h
+GO
+
+-- Vista 9 - Ranking de aspectos mejor y peor valorados
+
+CREATE VIEW BI_Ranking_de_aspectos_mejor_y_peor_valorados AS
+SELECT
+    h.Aspecto,
+    t.Cuatrimestre,
+    AVG(h.Puntaje) promedio_puntaje
+FROM Hecho_Valoracion_Encuesta h
+JOIN DIM_Tiempo t on t.ID = h.Tiempo
+GROUP BY h.Aspecto, t.Cuatrimestre
+GO
+
+-- Vista 10 - Satisfacción promedio por agente
+
+CREATE VIEW BI_SatisfaccionPromedioPorAgente AS
+SELECT 
+    h.RangoEtarioAgente,
+    t.Mes,
+    AVG(h.Puntaje) promedio_puntaje
+FROM Hecho_Valoracion_Encuesta h
+JOIN DIM_Tiempo t on h.Tiempo = t.ID
+GROUP BY h.RangoEtarioAgente, t.Mes
+GO
+
+select * from BI_Desvio_de_presupuesto
